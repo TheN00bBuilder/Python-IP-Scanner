7d53f79439b9b1be5078f09b28b4ff05bddf7c0e
TheN00bBuilder <41171807+TheN00bBuilder@users.noreply.github.com>
2018-07-12 23:55:26 -0500

    IP Scan English, Print Active to File
    This one's in English and prints the active IPs to a .txt file for analysis later.

---
+++ IP_Scanner_WALF.py
@@ -0,0 +1,90 @@
+import os
+import time
+import sys
+from datetime import datetime
+from subprocess import Popen
+
+devnull = open(os.devnull, 'wb')
+
+ipscanner_ico = '''
+#########################################################
+#       LOCAL NETWORK IP SCANNER - GH0ST S0FTWARE       #
+######################################################### 
+#                       CONTACT                         #
+#########################################################
+#              DEVELOPER : Ismail Tasdelen              #                       
+#      	       Translation; TheN00bBuilder		#
+#    Dev Mail Address : pentestdatabase@gmail.com	#
+#Translator Mail Address : then00bbuilder@overclocked.net#
+#Dev LINKEDIN : https://www.linkedin.com/in/ismailtasdelen#
+#      Dev  Whatsapp : + 90 534 295 94 31               #
+#########################################################
+'''
+
+print ipscanner_ico
+
+star = "**********************************************************************"
+
+print star
+print "This will print ALL active IPs to a file."
+ip_araligi_deger = raw_input("Choose a range of IPs to search. ( example: 192.168.0 ) ---> ")
+
+print star
+
+print "Scanned IP Range ",ip_araligi_deger 
+
+print star
+
+if ip_araligi_deger == "":
+ print star
+ print "Hooking on to process... hold on..."
+ print star
+
+p = []
+aktif = 0
+yanit_yok = 0
+pasif = 0
+
+for aralik in range(0,255):
+    ip = ip_araligi_deger + ".%d" % aralik
+    p.append((ip, Popen(['ping', '-c', '3', ip], stdout=devnull)))
+while p:
+    for i, (ip, proc) in enumerate(p[:]):
+        if proc.poll() is not None:
+            p.remove((ip, proc))
+            if proc.returncode == 0:
+		f = open("data.txt","a+")
+		f.write('IP Scan collected at {} \n '.format( datetime.now() )  )
+                f.write('%s Active \n' % ip)
+                aktif = aktif + 1
+		f.close()
+            elif proc.returncode == 2:
+                print('%s No Response' % ip)
+                aktif = yanit_yok + 1
+            else:
+                print('%s Inactive' % ip)
+                pasif = pasif + 1
+    time.sleep(.04)
+devnull.close()
+
+print star
+
+print "LOCAL NETWORK IP SCANNER. By GH0ST-SOFTWARE."
+
+print star
+
+import os
+
+print "Current Operating System",os.name
+print "Final Status Report"
+print "Active IP Count  [ ",aktif," ]"
+print "Inactive IP Count  [ ",pasif," ]"
+print "No Response Count  [ ",yanit_yok," ]"
+
+print star
+
+bitis_mesaj = "Scan complete. Happy hacking!"
+
+print bitis_mesaj
+print "All active IPs have been added to the data.txt file."
+print star
